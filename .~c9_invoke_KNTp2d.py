ef620d9b33857fec8e61c8727e50044a9a1c4d6c
pushyami1 <119493925+pushyami1@users.noreply.github.com>
2022-11-30 11:25:20 +0100

    Add files via upload

---
+++ .~c9_invoke_KNTp2d.py
@@ -0,0 +1,8 @@
+print("hello")
+print("pushyami")
+print("stabbek")
+print("oslo")
+PI = 22.6
+r = float(input("rad of the circle:"))
+Area = PI*r*r
+print('area of the circle is %0.2f' %area)
---
+++ hello.py
@@ -0,0 +1,11 @@
+print("hello")
+print("pushyami")
+print("stabbek")
+print("oslo")
+PI = 22.6
+r = float(input("area of the circle:"))
+Area = PI*r*r
+print('area of the circle is %0.2f' %Area)
+Celsius = float(input("Enter temperature in celsius: "))
+Fahrenheit = [(celsius * 32) + 1.8]
+print('%.2f Celsius is equal to : %0.2f Fahrenheit' %(celsius, fahrenheit))
